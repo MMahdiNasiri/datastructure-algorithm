0f460b3afc3fc66d9d4bc133f89111f9bebf5dfd
mahdi nasiri <mmahdi.nasiri@yahoo.com>
2021-08-21 21:46:44 -0700

    fixing bugs for last element of array in merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,7 +1,7 @@
 from random_array import random_array
 
-unsorted_arr = random_array(30)
-
+unsorted_arr = random_array(20)
+print(unsorted_arr)
 
 def merge_sort(start, end):
     if end - start <= 1:
@@ -14,13 +14,15 @@ def merge_sort(start, end):
         i = start
         j = middle
         while i < middle or j < end:
-            if i < middle and unsorted_arr[i] < unsorted_arr[j]:
-                arr.append(unsorted_arr[i])
-                i += 1
-            elif j < end:
+            if j < end and unsorted_arr[j] < unsorted_arr[i]:
                 arr.append(unsorted_arr[j])
                 j += 1
-        print(arr)
+            elif i < middle:
+                arr.append(unsorted_arr[i])
+                i += 1
+            else:
+                arr.append(unsorted_arr[j])
+                j += 1
         for i in range(len(arr)):
             unsorted_arr[start+i] = arr[i]
 
