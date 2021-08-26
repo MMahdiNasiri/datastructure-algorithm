64e9b7825cfbf7dd84c45af701bc57a07b0a6e95
mahdi nasiri <mmahdi.nasiri@yahoo.com>
2021-08-26 11:52:23 -0700

    improve random array to get maximum number

--- random_array.py
+++ random_array.py
@@ -1,9 +1,11 @@
 import random
 
 
-def random_array(num):
+def random_array(num, maxnum=None):
+    if maxnum is None:
+        maxnum = num
     unsorted_arr = []
     for x in range(num):
-        unsorted_arr.append(random.randint(0, num))
+        unsorted_arr.append(random.randint(0, maxnum))
 
     return unsorted_arr
