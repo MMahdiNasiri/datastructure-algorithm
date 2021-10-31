00e3b7f631f7b07fb60e306bc8d8ea0a5a693bde
mahdi nasiri <mmahdi.nasiri@yahoo.com>
2021-10-31 00:55:42 -0700

    add doubly to linkedlist

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -73,18 +73,21 @@ class DoublyLinkedList:
         self.head = None
 
     def insert_first(self, value):
-        node = self.insert_last(value)
-        self.head = node
+        new_node = DoublyNode(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
 
     def insert_last(self, value):
-        node = DoublyNode(value)
-        node.next = self.head
-        last_node = self.head.prev
-        if last_node is not None:
-            last_node.next = node
-        node.prev = last_node
-        node.next = self.head
-        return node
+        new_node = DoublyNode(value)
+        node = self.head
+        while node.next is not None:
+            node = node.next
+        node.next = new_node
+        new_node.prev = node
 
     def insert_middle(self, value, position):
         if position == 0:
@@ -93,7 +96,6 @@ class DoublyLinkedList:
             node = self.head
             new = DoublyNode(value)
             i = 1
-            # node.next is None when just have head
             while i < position and node.next is not None:
                 i += 1
                 node = node.next
@@ -101,9 +103,36 @@ class DoublyLinkedList:
             new.next = node.next
             new.prev = node
             node.next = new
+            new.next.prev = new
+
+    def pop(self):
+        node = self.head
+        if node.next is None:
+            self.head = None
+            return node.data_val
+        while node.next.next is not None:
+            node = node.next
+        last = node.next
+        node.next = None
+        return last.data_val
+
+    def key_delete(self, key):
+        node = self.head
+        while node.next is not None and node.next.data_val != key:
+            node = node.next
+        if node.next is None:
+            return None
+        else:
+            located_node = node.next
+            node.next = located_node.next
+            if located_node.next is not None:
+                located_node.next.prev = node
+            return located_node.data_val
 
     def print_list(self):
         node = self.head
         while node is not None:
-            print(node.data_val)
+            next_val = node.next.data_val if node.next is not None else None
+            prev_val = node.prev.data_val if node.prev is not None else None
+            print('node ', node.data_val, ', prev: ', prev_val, ', next: ', next_val)
             node = node.next
