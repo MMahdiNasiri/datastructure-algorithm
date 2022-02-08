20991cf1bf31c66f6a8e8f0637d3dd34bff1d5bf
Mahdi Nasiri <nasiri@bitpardaz.ir>
2022-02-08 05:16:32 -0800

    add new merge sort code

--- sorts/merge_sort.py
+++ sorts/merge_sort.py
@@ -29,3 +29,29 @@ def merge_sort(start, end):
 
 merge_sort(0, len(unsorted_arr))
 print(unsorted_arr)
+
+
+def merge(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        arr1 = merge(arr[:len(arr)//2])
+        arr2 = merge(arr[len(arr)//2:])
+        i = 0
+        j = 0
+        sorted_arr = []
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] < arr2[j]:
+                sorted_arr.append(arr1[i])
+                i += 1
+            else:
+                sorted_arr.append(arr2[j])
+                j += 1
+        while i < len(arr1):
+            sorted_arr.append(arr1[i])
+            i += 1
+
+        while j < len(arr2):
+            sorted_arr.append(arr2[j])
+            j += 1
+        return sorted_arr
